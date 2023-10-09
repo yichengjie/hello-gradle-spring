d5a3f110c927940ca5862540c8a39a2177586f2a
yichengjie <626659321@qq.com>
2023-10-09 18:35:17 +0800

    add gradle config

---
+++ start/webmvc/src/test/java/com/yicj/study/support/ConfigurationClassJUnitJupiterSpringTests.java
@@ -0,0 +1,14 @@
+package com.yicj.study.support;
+
+import com.yicj.study.config.WebMvcConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * @author yicj
+ * @date 2023年10月09日 18:26
+ */
+@SpringJUnitConfig(WebMvcConfig.class)
+public class ConfigurationClassJUnitJupiterSpringTests {
+
+
+}
